4045e5f66f1c2f0ea067d0dc6e2feefdb4d49729
ninadze1 <58101881+ninadze1@users.noreply.github.com>
2025-06-18 18:24:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+$('.carousel').slick({
+    infinite: true,
+    slidesToShow: 3,
+    prevArrow:'<img src="Arrow_left.svg">',
+    nextArrow:'<img src="Arrow_right.svg">',
+    slidesToScroll: 3
+  });
